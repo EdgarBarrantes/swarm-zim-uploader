21759c0bbfcab377ca4bdd21770955162e2115b7
Edgar Barrantes <edgar@barrantes.dev>
2022-03-17 15:21:44 +0200

    Move scripts to own folder

---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+ls home
+PATH_TO_UPLOADER="$(pwd)uploader.sh";
+echo $PATH_TO_UPLOADER
+sh $PATH_TO_UPLOADER;
---
+++ scripts/uploader.sh
@@ -0,0 +1,13 @@
+# # Download zim file.
+# # TODO: this should be received when running docker.
+# wget https://download.kiwix.org/zim/wikipedia/wikipedia_bm_all_maxi_2022-02.zim
+
+# # Get zim tools
+# wget https://download.openzim.org/release/zim-tools/zim-tools_linux-x86_64-3.1.0.tar.gz -O zimtools.tar.gz;
+# tar xvzf zimtools.tar.gz;
+
+# ./zim-tools_linux-x86_64-3.1.0/zimdump dump --dir=./wiki  ./wikipedia_bm_all_maxi_2022-02.zim
+
+# python3 indexCreator.py && cd wiki && tar -cf ../wiki.tar . && cd .. && rm -rf wiki
+curl -s bee-1:1635/peers | echo
+echo "DID IT!";
